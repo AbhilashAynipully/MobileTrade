c3e63ed23a19af6c9eb0641b324e13c7d234c3e2
Abhilash Aynipully <abhidigitally@gmail.com>
2023-11-07 15:41:01 +0000

    Bug Fix : Updated Allowed Host Heroku URL

--- mobiletrade/settings.py
+++ mobiletrade/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mobiletrade.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['mobiletrade-2f5fcc81b06c.herokuapp.com', 'localhost']
 
 
 # Application definition
