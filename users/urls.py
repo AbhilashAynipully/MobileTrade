11466c9f57312126bdd31a2f3c5dc4dc84cce7cb
Abhilash Aynipully <abhidigitally@gmail.com>
2023-11-13 17:57:07 +0000

    Create login.html template

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.get_home_page,name='home'),
+    path('login',views.get_login_page,name='login'),
+    path('logout',views.get_logout_page,name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
 # Create your views here.
 
 def get_home_page(request):
     return render(request,'../templates/index.html')
+
+
+def get_login_page(request):
+    return render(request,'../templates/login.html')
+
+
+def get_logout_page(request):
+    pass
