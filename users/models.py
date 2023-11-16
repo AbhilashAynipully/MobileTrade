6c74b465c6aa14883bd6217759f7116956417538
Abhilash Aynipully <abhidigitally@gmail.com>
2023-11-16 14:21:13 +0000

    Add image field to profile model under models.py

--- users/models.py
+++ users/models.py
@@ -1,16 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.conf import settings
 
 
 # Profile Model
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='user-placeholder.jpeg', upload_to='profile_pics')
+    profile_picture = models.ImageField(upload_to='users/', blank=True, null=True)
 
     def __str__(self):
         return self.user.username
 
+    @property
+    def user_picture(self):
+        if self.profile_picture:
+            url = self.profile_picture.url
+        else:
+            url = (
+                settings.STATIC_URL +
+                'images/users/user-placeholder.jpeg’
+            )
+        return url
 
 # Profile creation on new Signup
 def create_profile(sender,instance, created, **kwargs):
