eaaa28bec1a55299be1f8303905ce73c16e5e3f1
Abhilash Aynipully <abhidigitally@gmail.com>
2023-11-17 20:26:35 +0000

    Add password reset urls from auth_view to urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from . import views
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',views.get_home_page,name='home'),
@@ -9,4 +10,12 @@ urlpatterns = [
     path('profile/<int:pk>',views.get_user_profile,name='profile'),
     path('delete-account',views.delete_account,name='delete-account'),
     path('profile-update',views.get_profile_update,name='profile-update'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+    template_name='password_reset.html'), name='password_reset'),
+    path('password-reset/sent/', auth_views.PasswordResetDoneView.as_view(
+    template_name='password_reset_sent.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+    template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+    template_name='password_reset_complete.html'), name='password_reset_complete'),
 ]
