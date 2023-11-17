fbd6ffd0e65bd588d91500d5b35eb52855ce509f
Abhilash Aynipully <abhidigitally@gmail.com>
2023-11-17 09:31:21 +0000

    Create UserUpdateForm class function in forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from .models import Profile
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
@@ -18,3 +19,18 @@ class RegistrationForm(UserCreationForm):
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['password1'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['class'] = 'form-control '
+
+
+class UserUpdateForm(forms.ModelForm) :
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    first_name = forms.CharField(max_length=50,widget=forms.TextInput(attrs={'class':'form-control '}))
+    last_name = forms.CharField(max_length=50,widget=forms.TextInput(attrs={'class':'form-control '}))
+
+    class Meta:
+        model = User
+        fields = ('username','first_name','last_name','email')
+
+class ProfileUpdateForm(forms.ModelForm) :
+    class Meta:
+        model = Profile
+        fields = ['profile_picture']
