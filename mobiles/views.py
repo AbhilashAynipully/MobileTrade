0a5eda63da7b9aa9a24cc2edb1e0c9678f315d3b
Abhilash Aynipully <abhidigitally@gmail.com>
2023-11-20 13:56:31 +0000

    Create view get_all_mobiles to list all mobiles basis filter

--- mobiles/views.py
+++ mobiles/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from .models import Mobile, Favourite
+from . import choices
+from .forms import MobileAdditionForm, AddForm
+from .tools import filter_mobiles 
 
-# Create your views here.
+def get_all_mobiles(request):
+    mobiles = filter_mobiles(request)
+    mobiles, page_number =  pagination (request, mobiles)
+
+    context = {
+        'brand' : choices.BRANDS,
+        'mobiles' : mobiles, 
+        'values' : request.GET
+    }
+    render(request,'../templates/all_mobiles.html',context)
+
