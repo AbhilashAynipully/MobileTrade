031e275828cee78392a3cc2f78dd6040cf8e2382
Abhilash Aynipully <abhidigitally@gmail.com>
2023-11-20 18:55:49 +0000

    Update get_my_favourites view to pull user favourites

--- users/views.py
+++ users/views.py
@@ -97,9 +97,15 @@ def get_profile_update(request):
         messages.success(request,("You must be logged in to view this page!"))
         return redirect('login')
 
+
 def get_my_favourites(request):
     if request.user.is_authenticated:
-        return render(request,'../templates/my_favourites.html')
+    favourites = Favourite.objects.filter(seller=request.user).order_by('-created')
+    return render(request, '../templates/my_favourites.html', {'favourites':favourites})
+
+    else:
+        messages.success(request,("You must be logged in to view this page!"))
+        return redirect('login')
 
 
 def get_my_mobiles(request):
