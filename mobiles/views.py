eb037230a4ebf9ed09947210f33420e8386b21bb
Abhilash Aynipully <abhidigitally@gmail.com>
2023-11-21 06:28:01 +0000

    Update get_add_mobile view to redirect to home page

--- mobiles/views.py
+++ mobiles/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from .models import Mobile, Favourite
 from . import choices
 from .tool import filter_mobiles 
+from .forms import MobileAdditionForm
 
 def get_all_mobiles(request):
     mobiles = filter_mobiles(request)
@@ -27,7 +28,7 @@ def get_add_mobiles(request):
                 mobile.seller = request.user
                 mobile.save()
                 messages.success(request, 'Your mobile has been added successfully!')
-                return redirect('my_mobiles', request.user)
+                return redirect('home')
             else:
                 messages.error(request, 'Sorry your upload was unsuccessful! Please try again.')
         
