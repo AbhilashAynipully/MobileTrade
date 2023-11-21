9c699e8e361cc75f029e34ea45c26610ea7cd884
Abhilash Aynipully <abhidigitally@gmail.com>
2023-11-21 06:16:39 +0000

    Add get_add_mobiles view to views.py

--- mobiles/views.py
+++ mobiles/views.py
@@ -14,3 +14,21 @@ def get_all_mobiles(request):
     }
     return render(request,'../templates/all_mobiles.html',context)
 
+
+def get_add_mobiles(request):
+    if request.user.is_authenticated:
+        form = MobileAdditionForm()
+
+        if request.method == 'POST':
+            form = MobileAdditionForm(request.POST, request.FILES)
+
+            if form.is_valid():
+                mobile = form.save(commit=False)
+                mobile.seller = request.user
+                mobile.save()
+                messages.success(request, 'Your mobile has been added successfully!')
+                return redirect('my_mobiles', request.user)
+            else:
+                messages.error(request, 'Sorry your upload was unsuccessful! Please try again.')
+        
+        return render(request, '../templates/add_mobiles.html', {'form': form})
