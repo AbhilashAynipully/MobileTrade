9b9ea8a4d6f1c44724113e5518cdf09cd21ee112
Abhilash Aynipully <abhidigitally@gmail.com>
2023-11-21 12:04:36 +0000

    Update delte_mobile path

--- mobiles/urls.py
+++ mobiles/urls.py
@@ -4,5 +4,5 @@ from .import views
 urlpatterns = [
     path('search-mobiles/', views.get_all_mobiles, name='all_mobiles'),
     path('add-mobiles/', views.get_add_mobiles, name='add_mobiles'),
-    path('delete-mobiles/<str:pk>/', views.get_delete_mobiles, name='delete-mobiles'),
+    path('delete-mobiles/<int:pk>/', views.get_delete_mobiles, name='delete-mobiles'),
 ]
