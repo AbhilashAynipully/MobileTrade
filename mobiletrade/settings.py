b617b8a6485e2e3245b06a38d301d700999440e3
Abhilash Aynipully <abhidigitally@gmail.com>
2023-11-25 09:38:43 +0000

    Testing deployment with debug= False

--- mobiletrade/settings.py
+++ mobiletrade/settings.py
@@ -17,7 +17,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['mobiletrade-2f5fcc81b06c.herokuapp.com', os.environ.get("LOCAL_HOST_URL")]
 
