ed995aaa831da9b377a136e5e204819577298060
Abhilash Aynipully <abhidigitally@gmail.com>
2023-11-25 09:54:29 +0000

    Remove Crispy Forms from Installed Apps

--- mobiletrade/settings.py
+++ mobiletrade/settings.py
@@ -35,10 +35,8 @@ INSTALLED_APPS = [
     'cloudinary',
     'users',
     'mobiles',
-    'crispy_forms',
 ]
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4' 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
